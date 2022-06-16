b474cf7f9ecf22197d6ad175bacd48faf83a0d58
ZV1-nostah <95402305+ZV1-nostah@users.noreply.github.com>
2022-06-16 08:58:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = './media/icon2.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya2 = './media/icon3.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'zerotwo'
