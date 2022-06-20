167e830e0bf092e24e7485d685723054f8ad135f
ZV1-nostah <95402305+ZV1-nostah@users.noreply.github.com>
2022-06-20 09:49:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://wa.me/+6281229393997' //ubah jadi website lu, bisa link ig
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/38ca4dfb568101349fa58.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/44e52fdfdbd00abe61c36.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'zerotwo'
@@ -29,7 +29,7 @@ global.wm2 = '                     「 zerotwo botあ⁩ 」'
 global.wm3 = '⫹⫺ zex turu'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/2cb95a0db2d5ac3f2867c.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
